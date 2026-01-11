6b3731d726d8ee6b874dced26c70a713fd75b18f
savitar5049 <160987889+savitar5049@users.noreply.github.com>
2026-01-10 22:28:06 -0600

    Add files via upload

---
+++ svdden_banking/lang.lua
@@ -0,0 +1,158 @@
+Lang = {
+    client = {
+        bank_interact = '[E] - Bank',
+        atm_interact = '[E] - ATM',
+        cool_down = 'You must wait a moment before trying again',
+        myself_not_found = 'We could not find your user data, please try again later',
+        no_target = 'No target found',
+        no_money = 'You do not have enough money for this transaction',
+        min_amount = 'This action requires a minimum amount of $1',
+        transfer_success = 'Transfer successful',
+        cant_transfer_to_self = 'You cannot transfer money to yourself',
+        withdraw_success = 'Withdrawal successful',
+        deposit_success = 'Deposit successful',
+        invalid_data = 'Invalid data provided to from client',
+        uncaught_error = 'An uncaught error has occurred, please contact discord.gg/svdden',
+    },
+    UI = {
+        currency = '$',
+        navbar = {
+            account_overview_text = 'Overview',
+            accounts_labels = {
+                home = 'Home',
+                personal = 'Personal',
+                savings = 'Savings',
+            },
+            creditscore = {
+                title = 'Credit Score',
+                innertitle = 'Credit',
+                creditscoresubtext = 'Check regularly to monitor and improve.',
+                low = 'Low',
+                good = 'Good',
+                high = 'High',
+            },
+            account_text = 'Your Account',
+            logout = 'Logout',
+        },
+        home_page = {
+            card_section = {
+                title = 'Your Card',
+                subtext = 'Manage your card and monitor your spending at a glance.',
+                cardNumber = 'Card Number',
+                date = 'Date',
+                cvv = 'CVV',
+            },
+            quick_transfer = {
+                title = 'Quick Transfer',
+                subtext = 'Send funds instantly with just a few clicks.',
+                transferOptions = {
+                    {
+                        text1 = 'Recipient',
+                        text2 = 'Enter A ID',
+                    },
+                    {
+                        text1 = 'Amount',
+                    },
+                    {
+                        text1 = 'Recents',
+                    }
+                }
+            },
+            spending = {
+                title = 'Your Spending',
+                subtext = 'Track your daily spending and stay in control of your finances.',
+                daily = 'Daily',
+            },
+            transactons = {
+                title = 'Transactions',
+                types = {
+                    text1 = 'Type',
+                    text2 = 'Amount',
+                    text3 = 'Status',
+                    text4 = 'Date',
+                }
+            }
+        },
+        personal_page = {
+            title = 'Main Information',
+            subtext = 'Your account Information and actions you can take.',
+            personal_info = {
+                user_header = 'Account',
+                cardType = 'Personal',
+                cardNumber = 'Card Number',
+                date = 'Date',
+                cvv = 'CVV',
+            },
+            transactions = {
+                title = 'Transactions',
+                types = {
+                    text1 = 'Type',
+                    text2 = 'Amount',
+                    text3 = 'Status',
+                    text4 = 'Date',
+                }
+            },
+            actions = {
+                title = 'Actions',
+                subtext = 'Quick actions you can take to manage your account.',
+                menus = {
+                    headers = {
+                        'Withdraw',
+                        'Deposit',
+                        'Transfer',
+                    },
+                    text1 = 'Withdraw',
+                    text2 = 'Deposit',
+                    text3 = 'Transfer',
+                    text4 = 'Amount',
+                    text5 = 'Recipient ID',
+                    text6 = 'Your Account'
+                }
+            },
+            statistics = {
+                title = 'Statistics',
+            }
+        },
+        savings_page = {
+            account = {
+                title = 'Savings Account',
+                subtext = 'Your savings account information and actions you can take.',
+                personal_info = {
+                    cardNumber = 'Card Number',
+                    date = 'Date',
+                    cvv = 'CVV',
+                },
+            },
+            quick_transfer = {
+                title = 'Quick Transfer',
+                subtext = 'Send funds instantly to your personal account.',
+                transferOptions = {
+                    {
+                        text1 = 'Withdraw',
+                        text2 = 'Amount',
+                        text3 = 'Your Savings'
+                    },
+                    {
+                        text1 = 'Deposit',
+                        text2 = 'Amount',
+                        text3 = 'Your Savings'
+                    }
+                }
+            },
+            savings_history = {
+                title = 'Savings History',
+                subtext = 'Track how much your savings have grown over time.',
+                daily = 'Daily',
+            },
+            transactions = {
+                title = 'Transactions',
+                types = {
+                    text1 = 'Type',
+                    text2 = 'Amount',
+                    text3 = 'Status',
+                    text4 = 'Date',
+                }
+            },
+        }
+    }
+}
